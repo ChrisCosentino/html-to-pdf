b501c896d28b471d7d3e109c6c959688b8493f14
ChrisCosentino <chriscosentino@hotmail.com>
2024-02-06 20:21:46 -0500

    fix build

--- index.js
+++ index.js
@@ -12,8 +12,20 @@ app.post('/pdf', async (req, res) => {
 
   console.log(req.body);
 
+  // const browser = await puppeteer.launch({
+  //   headless: true,
+  // });
   const browser = await puppeteer.launch({
-    headless: false,
+    args: [
+      '--disable-setuid-sandbox',
+      '--no-sandbox',
+      '--single-process',
+      '--no-zygote',
+    ],
+    executablePath:
+      process.env.NODE_ENV === 'production'
+        ? process.env.PUPPETEER_EXECUTABLE_PATH
+        : puppeteer.executablePath(),
   });
 
   const page = await browser.newPage();
