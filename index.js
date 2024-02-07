149e58b5529aa864808455b70a99b734bd4fe4f8
ChrisCosentino <chriscosentino@hotmail.com>
2024-02-06 20:04:24 -0500

    added pdf

--- index.js
+++ index.js
@@ -6,8 +6,22 @@ const app = express();
 
 const PORT = process.env.PORT || 4000;
 
-app.get('/pdf', (req, res) => {
-  pdfLogic(res);
+app.post('/pdf', async (req, res) => {
+  // pdfLogic(res);
+
+  console.log(req.body);
+
+  const browser = await puppeteer.launch({
+    headless: false,
+  });
+
+  const page = await browser.newPage();
+
+  await page.setContent(req.body.html, { waitUntil: 'networkidle0' });
+
+  const pdfBuffer = await page.pdf({ format: 'A4' });
+
+  return res.send(pdfBuffer);
 });
 
 app.get('/scrape', (req, res) => {
